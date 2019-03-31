a8205169513a21a1a742ed40b59eee77c4e4a1f8
Zixuan Yao <yaozixuan@users.noreply.github.com>
2019-03-31 14:17:52 +0800

    GUI Update 1

--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+    w.setWindowTitle("BrainLine");
+    w.setWindowIcon(QIcon("logo.jpg"));
     w.show();
 
     return a.exec();
